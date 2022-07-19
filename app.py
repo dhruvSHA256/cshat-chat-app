d5ec553f9a9ac1e70cb91094a10d1d05dfa45db0
DHruv <dhruv.sha256@gmail.com>
2022-07-20 01:46:32 +0530

    user model with unique username

--- app.py
+++ app.py
@@ -1,16 +1,33 @@
 from flask import Flask, render_template
-from wtform_fields import *
+from models import User, SQLAlchemy
+from wtform_fields import RegistrationForm
+import os
 
 # config flask app
 app = Flask(__name__)
-app.secret_key = 'replace later'
+app.secret_key = "replace later"
 
-@app.route('/', methods=['GET', 'POST'])
+# config postgres db
+app.config['SQLALCHEMY_DATABASE_URI'] = os.environ['DB_URL'].replace(
+    "postgres", "postgresql")
+db = SQLAlchemy(app)
+
+
+# main route
+@app.route("/", methods=["GET", "POST"])
 def index():
     reg_form = RegistrationForm()
+
+    # if form is validated
     if reg_form.validate_on_submit():
-        return "Success"
-    return render_template("index.html",form=reg_form)
+        username = reg_form.username.data
+        password = reg_form.password.data
+        user = User(username=username, password=password)
+        db.session.add(user)
+        db.session.commit()
+        return "inserted into db"
+
+    return render_template("index.html", form=reg_form)
 
 
 if __name__ == "__main__":
--- wtform_fields.py
+++ wtform_fields.py
@@ -1,9 +1,42 @@
 from flask_wtf import FlaskForm
-from wtforms import StringField, PasswordField, SubmitField
+from wtforms import StringField, PasswordField, SubmitField, ValidationError
 from wtforms.validators import InputRequired, Length, EqualTo
+from models import User
+
 
 class RegistrationForm(FlaskForm):
-    username = StringField('username',validators=[InputRequired(message="Username Required"),Length(min=4,max=10,message="Username must between 4 and 10 characters long")])
-    password = PasswordField('password',validators=[InputRequired(message="Password Required"),Length(min=8,max=25,message="Password must between 8 and 25 characters long")])
-    cnfrm_password = PasswordField('cnfrm_password',validators=[InputRequired(message="Username Required"),EqualTo('password',"Passwords are not same")])
-    submit_button = SubmitField('Create')
+    username = StringField(
+        "username",
+        validators=[
+            InputRequired(message="Username Required"),
+            Length(
+                min=4, max=10,
+                message="Username must between 4 and 10 characters long"
+            ),
+        ],
+    )
+    password = PasswordField(
+        "password",
+        validators=[
+            InputRequired(message="Password Required"),
+            Length(
+                min=8, max=25,
+                message="Password must between 8 and 25 characters long"
+            ),
+        ],
+    )
+    cnfrm_password = PasswordField(
+        "cnfrm_password",
+        validators=[
+            InputRequired(message="Username Required"),
+            EqualTo("password", "Passwords are not same"),
+        ],
+    )
+    submit_button = SubmitField("Create")
+
+    # custom validator for unique username
+
+    def validate_username(self, username):
+        user_object = User.query.filter_by(username=username.data).first()
+        if user_object:
+            raise ValidationError("Username already exists")
